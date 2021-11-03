d2f101f50daefd10fc32324592620a121b7b99fc
Yinhui Wu <254318164@qq.com>
2021-11-03 14:04:16 -0400

    update

--- Mini GAME.py
+++ Mini GAME.py
@@ -1,14 +1,7 @@
-# In this implementation a card (that is not a 10) is represented
-# by a 2 character string, where the 1st character represents a rank and the 2nd a suit.
-# Each card of rank 10 is represented as a 3 character string, first two are the rank and the 3rd is a suit.
-
 import random
 from typing import Counter
 
 def wait_for_player():
-    '''()->None
-    Pauses the program until the user presses enter
-    '''
     try:
          input("\nPress enter to continue. ")
          print()
@@ -17,20 +10,15 @@ def wait_for_player():
 
 
 def make_deck():
-    '''()->list of str
-        Returns a list of strings representing the playing deck,
-        with one queen missing.
-    '''
     deck=[]
     suits = ['\u2660', '\u2661', '\u2662', '\u2663']
     ranks = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
     for suit in suits:
         for rank in ranks:
             deck.append(rank+suit)
-    deck.remove('Q\u2663') # remove a queen as the game requires
+    deck.remove('Q\u2663') 
     return deck
 
-# print(make_deck())
 
 
 def shuffle_deck(deck):
@@ -43,15 +31,6 @@ def shuffle_deck(deck):
 #####################################
 
 def deal_cards(deck):
-     '''(list of str)-> tuple of (list of str,list of str)
-
-     Returns two lists representing two decks that are obtained
-     after the dealer deals the cards from the given deck.
-     The first list represents dealer's i.e. computer's deck
-     and the second represents the other player's i.e user's list.
-     '''
-     # COMPLETE THE BODY OF THIS FUNCTION ACCORDING TO THE DESCRIPTION ABOVE
-     # YOUR CODE GOES HERE
      dealer=[]
      other=[]
      cards = random.randint(25,26)
@@ -65,31 +44,10 @@ def deal_cards(deck):
      return (dealer, other)
 
  
-# print(deal_cards(make_deck()))
+
 
 def remove_pairs(l):
-    '''
-     (list of str)->list of str
-
-     Returns a copy of list l where all the pairs from l are removed AND
-     the elements of the new list shuffled
-
-     Precondition: elements of l are cards represented as strings described above
-
-     Testing:
-     Note that for the individual calls below, the function should
-     return the displayed list but not necessarily in the order given in the examples.
-
-     >>> remove_pairs(['9♠', '5♠', 'K♢', 'A♣', 'K♣', 'K♡', '2♠', 'Q♠', 'K♠', 'Q♢', 'J♠', 'A♡', '4♣', '5♣', '7♡', 'A♠', '10♣', 'Q♡', '8♡', '9♢', '10♢', 'J♡', '10♡', 'J♣', '3♡'])
-     ['10♣', '2♠', '3♡', '4♣', '7♡', '8♡', 'A♣', 'J♣', 'Q♢']
-     >>> remove_pairs(['10♣', '2♣', '5♢', '6♣', '9♣', 'A♢', '10♢'])
-     ['2♣', '5♢', '6♣', '9♣', 'A♢']
-    '''
-
     
-    
-    # COMPLETE THE BODY OF THIS FUNCTION ACCORDING TO THE DESCRIPTION ABOVE
-    # YOUR CODE GOES HERE
     non_pairs=[]
 
     l.sort()
@@ -97,12 +55,12 @@ def remove_pairs(l):
     while i<len(l)-1:
         card1=l[i]
         card2=l[i+1]
-        if card1[:-1] == card2[:-1]: # compare the number if they are the same without suits.
+        if card1[:-1] == card2[:-1]: 
             i=i+1           
         else: 
             non_pairs.append(l[i])
         i=i+1
-    if i==len(l)-1: #  true if the last card is not a part of a pair
+    if i==len(l)-1: 
         non_pairs.append(l[i])
 
     random.shuffle(non_pairs)
@@ -111,7 +69,7 @@ def remove_pairs(l):
     
     
 
-#print(remove_pairs(['10♣', '2♣', '5♢', '6♣', '9♣', 'A♢', '10♢']))
+
 
 
 
@@ -119,17 +77,13 @@ def remove_pairs(l):
 
 
 def print_deck(deck):
-    '''
-    (list)-None
-    Prints elements of a given list deck separated by a space
-    '''
+    
     for _ in deck:
         print( _,end=" ")
     
     
     
-    # COMPLETE THE BODY OF THIS FUNCTION ACCORDING TO THE DESCRIPTION ABOVE
-    # YOUR CODE GOES HERE
+    
 
 
 
@@ -141,16 +95,6 @@ def print_deck(deck):
 
 
 def get_valid_input(n):
-     '''
-     (int)->int
-     Returns an integer given by the user that is at least 1 and at most n.
-     Keeps on asking for valid input as long as the user gives integer outside of the range [1,n]
-     
-     Precondition: n>=1
-     '''
-
-     # COMPLETE THE BODY OF THIS FUNCTION ACCORDING TO THE DESCRIPTION ABOVE
-     # YOUR CODE GOES HERE
      print(f"\nI have {len(n)} cards. If 1 stands for my first card and \n{len(n)} for my last card, which of my cards would you like?")
      number = int(input(f"Give me an integer between 1 and {len(n)}:"))
      while number <1 or number >len(n):
@@ -158,13 +102,11 @@ def get_valid_input(n):
         number = int(input(f"Give me an integer between 1 and {len(n)}:"))
      return number
 
-#print(get_valid_input(['10♣', '2♣', '5♢', '6♣', '9♣', 'A♢', '10♢']))
+
 
 
 
 def play_game():
-     '''()->None
-     This function plays the game'''
 
             
      deck=make_deck()
@@ -187,8 +129,7 @@ def play_game():
      dealer=remove_pairs(dealer)
      human=remove_pairs(human)
 
-     # COMPLETE THE play_game function HERE
-     # YOUR CODE GOES HERE
+     
     
      # human turn
      print("\n***********************************************************")
---
+++ project.py
@@ -0,0 +1,63 @@
+#Gourp: YinHui WU 1830057  and GuoShan Gan 1830069
+
+# Question 1
+import turtle as t
+import random
+
+def changePenColor(): # set pen color change
+    t.colormode(255) # color format
+    a = random.randint(100,255) # set a color and color in range 
+    b = random.randint(100,255)
+    c = random.randint(100,255)
+    t.pencolor(a,b,c)  # pen colors
+
+def shape(): # shape going to Draw
+    t.pensize(10) # set pensize
+    while True: # set a loop
+        changePenColor() # Bring in changepencolor to here(the equation for changing the pen color set before)
+        t.fd(200)
+        t.speed(10)
+        t.right(111)
+
+shape()
+
+
+
+
+# Question 2 
+import turtle as t
+import random
+
+def changePenColor(): #set color change
+    t.colormode(255)
+    a = random.randint(100,255)
+    b = random.randint(100,255)
+    c = random.randint(100,255)
+    t.pencolor(a,b,c)
+
+def fd():   # set moving forward
+    t.fd(100)
+
+def fdr():   # set turn right 90 degree and moving forward
+    t.right(90)
+    t.fd(100)
+
+def fdl():  # set turn left 90 degree and movinng forward
+    t.left(90)
+    t.fd(100)
+
+def bk():  # set moving backward
+    t.bk(100)
+    
+
+def onkey(): #  Set keyboard operation
+    t.pensize(10) 
+    while True:  # using loop 
+        changePenColor() 
+        t.onkey(fd,"Up") # press up going forward 
+        t.onkey(fdr,"Right")
+        t.onkey(fdl,"Left")
+        t.onkey(bk,"Down")
+        t.listen()
+
+onkey()
